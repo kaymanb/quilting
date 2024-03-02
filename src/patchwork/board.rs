c6cd1b1cd0b9a986c4ee61dbbb83717b7061c69d
Kayman Brusse <kayman.brusse@zeitview.com>
2024-03-02 11:29:38 -0500

    Better visualization of game board

--- src/patchwork/board.rs
+++ src/patchwork/board.rs
@@ -1,14 +1,66 @@
 use std::collections::HashMap;
 use std::fmt;
 
+use colored::{ColoredString, Colorize};
 use geo::{coord, point, Contains, Point, Rect};
 
 use crate::patchwork::Patch;
 
+struct Viewer<'a, 'b> {
+    out: &'a mut fmt::Formatter<'b>,
+}
+
+impl<'a, 'b> Viewer<'a, 'b> {
+    const WIDTH_MULTIPLE: usize = 2;
+
+    const VERTICAL: &'static str = "│";
+    const HORIZONTAL: &'static str = "─";
+
+    const TOP_LEFT: &'static str = "╭";
+    const TOP_RIGHT: &'static str = "╮";
+    const BOTTOM_LEFT: &'static str = "╰";
+    const BOTTOM_RIGHT: &'static str = "╯";
+
+    fn new(out: &'a mut fmt::Formatter<'b>) -> Self {
+        Self { out }
+    }
+
+    fn top(&mut self, width: usize) -> Result<(), fmt::Error> {
+        let left = Viewer::TOP_LEFT;
+        let right = Viewer::TOP_RIGHT;
+        let middle = Viewer::HORIZONTAL.repeat(width * Viewer::WIDTH_MULTIPLE);
+        let top = Viewer::colorize(format!("{left}{middle}{right}"));
+        writeln!(self.out, "{top}")
+    }
+
+    fn bottom(&mut self, width: usize) -> Result<(), fmt::Error> {
+        let left = Viewer::BOTTOM_LEFT;
+        let right = Viewer::BOTTOM_RIGHT;
+        let middle = Viewer::HORIZONTAL.repeat(width * Viewer::WIDTH_MULTIPLE);
+        let bottom = Viewer::colorize(format!("{left}{middle}{right}"));
+        writeln!(self.out, "{bottom}")
+    }
+
+    fn row(&mut self, tiles: Vec<ColoredString>) -> Result<(), fmt::Error> {
+        let edge = Viewer::colorize(Viewer::VERTICAL.to_owned());
+        write!(self.out, "{edge}")?;
+        for tile in tiles.iter() {
+            for _ in 0..Viewer::WIDTH_MULTIPLE {
+                write!(self.out, "{tile}")?;
+            }
+        }
+        writeln!(self.out, "{edge}")
+    }
+
+    fn colorize(string: String) -> ColoredString {
+        string.bright_black()
+    }
+}
+
 #[derive(Debug)]
 pub struct Board {
-    width: usize,
     height: usize,
+    width: usize,
     geometry: Rect<usize>,
     placements: HashMap<Point<usize>, Patch>,
 }
@@ -33,11 +85,9 @@ impl Default for Board {
 
 impl fmt::Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let edge = "-".repeat(self.width);
-        writeln!(f, "+{edge}+")?;
-
+        let mut rows = Vec::new();
         for y in (0..self.height).rev() {
-            let mut row = String::new();
+            let mut row = Vec::new();
             for x in 0..self.width {
                 // Sample is offset by 0.5 to avoid boundary issues
                 let mut found = false;
@@ -45,17 +95,22 @@ impl fmt::Display for Board {
                 for patch in self.placements.values() {
                     if patch.geometry.contains(&sample) {
                         found = true;
-                        row.push('*');
+                        row.push("█".blue());
                         break;
                     }
                 }
                 if !found {
-                    row.push(' ');
+                    row.push(" ".into());
                 }
             }
-            writeln!(f, "|{row}|")?;
+            rows.push(row);
         }
 
-        writeln!(f, "+{edge}+")
+        let mut viewer = Viewer::new(f);
+        viewer.top(self.width)?;
+        for row in rows.into_iter() {
+            viewer.row(row)?;
+        }
+        viewer.bottom(self.width)
     }
 }
